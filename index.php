ef1222a0397b2e4b4484cbc5ba8f3556bb791646
eduardosenacc <eduardomantovani999@gmail.com>
2023-12-04 21:34:28 -0300

    css

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="css/style.css" media="screen"/>
+  <link rel="stylesheet" type="text/css" href="css/style_login.css" media="screen"/>
   <title>Página de Login</title>
 </head>
 <body>
